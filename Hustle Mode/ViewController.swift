deb26887dea020ef80a56c25f7ee0caa13520ac7
user148657 <user148657@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-12-26 03:09:23 -0500

    created view with assets

--- Hustle Mode/ViewController.swift
+++ Hustle Mode/ViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var darkBlueBG: UIImageView!
+    @IBOutlet weak var powerButton: UIButton!
+    @IBOutlet weak var cloudHolder: UIView!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var hustleLbl: UILabel!
+    @IBOutlet weak var activatedLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //when power button is pressed//
+    @IBAction func powerButtonPushed(_ sender: Any) {
+        cloudHolder.isHidden = false
+        powerButton.isHidden = true
+        darkBlueBG.isHidden = true
+    }
+    
 
 
 }
